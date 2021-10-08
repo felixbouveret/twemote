f5ce88be1347acd51650345699ae7f8ba271ae6c
Benoit Lamonica <ben33127@gmail.com>
2021-10-08 17:38:50 +0200

    Exclamation

--- src/static/index.js
+++ src/static/index.js
@@ -1,12 +1,13 @@
 // Static const
 const SLACK_PREFIX = ":alphabet-yellow-#:"
-const EXCLUDED_SYMBOL = ["'", ",", ".", "!", ":", ";", "\n"]
+const EXCLUDED_SYMBOL = ["'", ",", ".", ":", ";", "\n"]
 const MAIN_INPUT = document.querySelector("textarea")
 const RESULT_INPUT = document.querySelector(".result")
 const COPY_BUTTON = document.querySelector('#copy')
 const COPIED = "Copié dans le presse Papier"
 const SYMBOL_DICO = {
-    "?": "question"
+    "?": "question",
+    "!": "exclamation"
 }
 
 const ENV = {
