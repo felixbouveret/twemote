9e7fd04f38c96191a7f41a88e52227be45342c8d
Felix <bouveret.felix@gmail.com>
2021-10-08 16:40:36 +0200

    feat(indexjs): add button animation

--- src/utils/index.js
+++ src/utils/index.js
@@ -3,19 +3,28 @@
 import ENV from "../static";
 
 export function copyToClipboard(text) {
-    const elem = document.createElement('textarea');
-    elem.value = text;
-    document.body.appendChild(elem);
-    elem.select();
-    document.execCommand('copy');
-    document.body.removeChild(elem);
+  const elem = document.createElement("textarea");
+  elem.value = text;
+  document.body.appendChild(elem);
+  elem.select();
+  document.execCommand("copy");
+  document.body.removeChild(elem);
 
-    let confMsg = document.createElement('strong')
-    confMsg.style.color = "red"
-    confMsg.textContent = ENV.COPIED
+  let confMsg = document.createElement("strong");
+  confMsg.classList.add("successMessage");
+  confMsg.textContent = ENV.COPIED;
 
-    document.querySelector('body').appendChild(confMsg)
-    setTimeout(() => {
-        document.querySelector('body').removeChild(confMsg)
-    }, 1000)
+  document.querySelector("body").appendChild(confMsg);
+  setTimeout(() => {
+    confMsg.classList.add("successMessageEnter");
+  }, 100);
+
+  document.querySelector("body").appendChild(confMsg);
+  setTimeout(() => {
+    confMsg.classList.remove("successMessageEnter");
+  }, 1900);
+
+  setTimeout(() => {
+    document.querySelector("body").removeChild(confMsg);
+  }, 2000);
 }
