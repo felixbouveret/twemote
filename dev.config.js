a085beeb8039aa56d3744e9ab818afbed44a7ab9
Felix <bouveret.felix@gmail.com>
2021-10-10 20:27:22 +0200

    feat(config): add first draft for images loading

--- dev.config.js
+++ dev.config.js
@@ -1,51 +1,58 @@
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    mode: "development",
-    watch: true,
-    entry: ['./src/index.js', './src/style/main.scss'],
-    output: {
-        path: path.resolve('./dist'),
-        filename: 'main.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '/style/[name].css',
-                        }
-                    },
-                    {
-                        loader: 'extract-loader'
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'postcss-loader'
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
+  mode: "development",
+  watch: true,
+  entry: ["./src/index.js", "./src/style/main.scss"],
+  output: {
+    path: path.resolve("./dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "/style/[name].css",
             },
+          },
+          {
+            loader: "extract-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "file-loader",
+        options: {
+          name: "/images/[name].[ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
 
-        new BrowserSyncPlugin({
-            host: 'localhost',
-            port: 5000,
-            server: { baseDir: ['dist'] }
-        })
-    ]
-}
+    new BrowserSyncPlugin({
+      host: "localhost",
+      port: 5000,
+      server: { baseDir: ["dist"] },
+    }),
+  ],
+};
--- prod.config.js
+++ prod.config.js
@@ -1,45 +1,52 @@
 const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
-    mode: "development",
-    watch: false,
-    entry: ['./src/index.js', './src/style/main.scss'],
-    output: {
-        path: path.resolve('./dist'),
-        filename: 'main.js',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    {
-                        loader: 'file-loader',
-                        options: {
-                            name: '/style/[name].css',
-                        }
-                    },
-                    {
-                        loader: 'extract-loader'
-                    },
-                    {
-                        loader: 'css-loader'
-                    },
-                    {
-                        loader: 'postcss-loader'
-                    },
-                    {
-                        loader: 'sass-loader'
-                    }
-                ]
+  mode: "production",
+  watch: false,
+  entry: ["./src/index.js"],
+  output: {
+    path: path.resolve("./dist"),
+    filename: "main.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "style/[name].css",
             },
+          },
+          {
+            loader: "extract-loader",
+          },
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "postcss-loader",
+          },
+          {
+            loader: "sass-loader",
+          },
         ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-    ]
-}
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loader: "file-loader",
+        options: {
+          name: "/images/[name].[ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
